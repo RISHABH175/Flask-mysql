b955b956421ca6d82f232521cc05877cfe6ef345
Rishabh Shyam Sunder <60140002+RISHABH175@users.noreply.github.com>
2023-03-16 13:55:42 +0000

    testing

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+# import pymysql
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'ap-south.connect.psdb.cloud'
+app.config['MYSQL_USER'] = 'o1u9brd68nj795lm4a42'
+app.config[
+  'MYSQL_PASSWORD'] = 'pscale_pw_PAVYQAd5I6zfKZtRJmto3PdVgximyKYGJzp4fzlahyW'
+app.config['MYSQL_DB'] = 'rishabhcareers'
+
+mysql = MySQL(app)
+
+# def sql_connector():
+#   conn = pymysql.connect(
+#     user='o1u9brd68nj795lm4a42',
+#     password='pscale_pw_PAVYQAd5I6zfKZtRJmto3PdVgximyKYGJzp4fzlahyW',
+#     db='rishabhcareers',
+#     host='ap-south.connect.psdb.cloud',
+#     ssl_ca="/etc/ssl/cert.pem")
+#   c = conn.cursor()
+#   return conn, c
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  if request.method == 'POST':
+    title = request.form.get('title')
+    location = request.form.get('location')
+    salary = request.form.get('salary')
+    currency = request.form.get('currency')
+    responsibilities = request.form.get('responsibilities')
+    requirements = request.form.get('requirements')
+
+    cursor = mysql.connection.cursor()
+    cursor.execute(
+      ''' INSERT INTO jobs VALUES(%s,%s,%s,%s,%s,%s)''',
+      (title, location, salary, currency, responsibilities, requirements))
+    mysql.connection.commit()
+    cursor.close()
+    return f"Done!!"
+    # conn, c = sql_connector()
+    # c.execute("INSERT INTO jobs VALUES ('{}' {} {} {} {} {})".format(
+    #   title, location, salary, currency, responsibilities, requirements))
+    # conn.commit()
+    # conn.close()
+    # c.close()
+
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0")
+
+# pymysql, mysql-connector, mysql-connector-python
