f7a8e832e0c69e48d62069a554bcc325ebed269d
Rishabh Shyam Sunder <60140002+RISHABH175@users.noreply.github.com>
2023-03-16 15:03:29 +0000

    Database changes

--- app.py
+++ app.py
@@ -19,33 +19,33 @@ mysql = MySQL(app)
 #     db='rishabhcareers',
 #     host='ap-south.connect.psdb.cloud',
 #     ssl_ca="/etc/ssl/cert.pem")
-#   c = conn.cursor()
-#   return conn, c
+#     c = conn.cursor()
+#     return conn, c
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-  if request.method == 'POST':
-    title = request.form('title')
-    location = request.form('location')
-    salary = request.form('salary')
-    currency = request.form('currency')
-    responsibilities = request.form('responsibilities')
-    requirements = request.form('requirements')
+  cursor = mysql.connection.cursor()
+  # if request.method == 'POST':
+  title = request.form('title')
+  location = request.form('location')
+  salary = request.form('salary')
+  currency = request.form('currency')
+  responsibilities = request.form('responsibilities')
+  requirements = request.form('requirements')
 
-    cursor = mysql.connection.cursor()
-    cursor.execute(
-      ''' INSERT INTO jobs VALUES(%s,%s,%s,%s,%s,%s)''',
-      (title, location, salary, currency, responsibilities, requirements))
-    mysql.connection.commit()
-    cursor.close()
-    return f"Done!!"
-    # conn, c = sql_connector()
-    # c.execute("INSERT INTO jobs VALUES ('{}' {} {} {} {} {})".format(
-    #   title, location, salary, currency, responsibilities, requirements))
-    # conn.commit()
-    # conn.close()
-    # c.close()
+  cursor.execute(
+    "INSERT INTO jobs(title, location, salary, currency, responsibilities, requirements) VALUES(%s %s %s %s %s %s)",
+    (title, location, salary, currency, responsibilities, requirements))
+  mysql.connection.commit()
+  cursor.close()
+  return "success"
+  # conn, c = sql_connector()
+  # c.execute("INSERT INTO jobs VALUES ('{}' {} {} {} {} {})".format(
+  #   title, location, salary, currency, responsibilities, requirements))
+  # conn.commit()
+  # conn.close()
+  # c.close()
 
   return render_template('index.html')
 
